00a6e610e4a81f61ca4598f9e400dd5d625cc2c3
Shark-vil <shark_vil@mail.ru>
2022-07-02 01:25:26 +0300

    Create templates for the "notebooks" table

---
+++ app/Http/Controllers/Api/NotebookController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class NotebookController extends Controller
+{
+	public function index()
+	{
+
+	}
+
+	public function show()
+	{
+
+	}
+
+	public function store()
+	{
+
+	}
+
+	public function update()
+	{
+
+	}
+
+	public function delete()
+	{
+
+	}
+}
---
+++ database/migrations/2022_07_01_215122_create_notebooks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotebooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notebooks', function (Blueprint $table) {
+            $table->id();
+						$table->string('full_name');
+						$table->string('company_name')->nullable();
+						$table->string('phone_number');
+						$table->string('email');
+						$table->date('date_of_birth')->nullable();
+						$table->string('photo_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notebooks');
+    }
+}
