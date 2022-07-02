5e308e5a5fb451a2afb8222c99a6be3c096abbc4
Shark-vil <shark_vil@mail.ru>
2022-07-02 04:39:37 +0300

    Removed extra field in the model, added photo saving function

--- app/Models/Notebook.php
+++ app/Models/Notebook.php
@@ -4,17 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Notebook extends Model
 {
-    use HasFactory;
+	use HasFactory;
 
-		protected $fillable = [
-			'full_name',
-			'company_name',
-			'phone_number',
-			'email',
-			'date_of_birth',
-			'photo_url'
+	private $photosDirectoryPath = 'notebook/photos';
+
+	protected $fillable = [
+		'full_name',
+		'company_name',
+		'phone_number',
+		'email',
+		'date_of_birth'
 	];
+
+	public function saveAvatarFromRequest(Request $request)
+	{
+		if ($request->hasFile('photo_file')) {
+			$fileRequest = $request->file('photo_file');
+			$fileExtension = $fileRequest->getClientOriginalExtension();
+			$fileNameToStore = $this->id . '.' . $fileExtension;
+			$fileRequest->storeAs('public/' . $this->photosDirectoryPath, $fileNameToStore);
+
+			if (empty($this->photo_url)) {
+				$this->photo_url = $this->photosDirectoryPath . '/' . $fileNameToStore;
+				$this->save();
+			}
+		}
+	}
 }
