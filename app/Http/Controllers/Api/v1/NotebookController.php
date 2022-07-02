1cc9601ebb2130b6e4d9498cd44992a63105e396
Shark-vil <shark_vil@mail.ru>
2022-07-02 04:40:05 +0300

    Fill in controller functions and set routes

--- app/Http/Controllers/Api/v1/NotebookController.php
+++ app/Http/Controllers/Api/v1/NotebookController.php
@@ -4,31 +4,43 @@ namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\Notebook\NotebookPostRequest;
+use App\Http\Requests\Notebook\NotebookUpdateRequest;
+use App\Models\Notebook;
+use App\Http\Resources\NotebookResource;
 
 class NotebookController extends Controller
 {
 	public function index()
 	{
-
+		return NotebookResource::collection(Notebook::all());
 	}
 
-	public function show()
+	public function show(int $id)
 	{
-
+		return new NotebookResource(Notebook::findOrFail($id));
 	}
 
-	public function store()
+	public function store(NotebookPostRequest $request)
 	{
-
+		$notebook = Notebook::create($request->all());
+		$notebook->saveAvatarFromRequest($request);
+		return new NotebookResource($notebook);
 	}
 
-	public function update()
+	public function update(NotebookUpdateRequest $request, int $id)
 	{
-
+		$notebook = Notebook::findOrFail($id);
+		$notebook->update($request->all());
+		$notebook->saveAvatarFromRequest($request);
+		return new NotebookResource($notebook);
 	}
 
-	public function delete()
+	public function destroy(int $id)
 	{
-
+		$notebook = Notebook::findOrFail($id);
+		$notebook->delete();
+		return response(200);
 	}
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\v1\NotebookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::prefix('v1/notebook')->group(function () {
+	// GET /api/v1/notebook/
+	Route::get('/', [NotebookController::class, 'index']);
+	// GET /api/v1/notebook/<id>/
+	Route::get('/{id}', [NotebookController::class, 'show']);
+	// POST /api/v1/notebook/
+	Route::post('/', [NotebookController::class, 'store']);
+	// POST /api/v1/notebook/<id>/
+	Route::post('/{id}', [NotebookController::class, 'update']);
+	// DELETE /api/v1/notebook/<id>/
+	Route::delete('/{id}', [NotebookController::class, 'destroy']);
 });
