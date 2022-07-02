5990029dc8c271913d865adf505d3d219ba416c5
Shark-vil <shark_vil@mail.ru>
2022-07-02 08:01:07 +0300

    Document formatting

--- app/Http/Resources/NotebookResource.php
+++ app/Http/Resources/NotebookResource.php
@@ -6,22 +6,22 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class NotebookResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
-    public function toArray($request)
-    {
-        return [
-					'id' => $this->id,
-					'full_name' => $this->full_name,
-					'company_name' => $this->company_name,
-					'phone_number' => $this->phone_number,
-					'email' => $this->email,
-					'date_of_birth' => $this->date_of_birth,
-					'photo_url' => is_string($this->photo_url) ? asset('storage/' . $this->photo_url) : null
-				];
-    }
+	/**
+	 * Transform the resource into an array.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+	 */
+	public function toArray($request)
+	{
+		return [
+			'id' => $this->id,
+			'full_name' => $this->full_name,
+			'company_name' => $this->company_name,
+			'phone_number' => $this->phone_number,
+			'email' => $this->email,
+			'date_of_birth' => $this->date_of_birth,
+			'photo_url' => is_string($this->photo_url) ? asset('storage/' . $this->photo_url) : null
+		];
+	}
 }
